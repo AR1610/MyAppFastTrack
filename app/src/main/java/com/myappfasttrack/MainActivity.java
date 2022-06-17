fd08fe8bdac9e40c853a2f8e3f5a715795406d38
Aarti Patel <royal.arati10@gmail.com>
2022-06-17 12:51:02 +0530

    Java Class findview by ID & Runtime Image change

--- app/src/main/java/com/myappfasttrack/MainActivity.java
+++ app/src/main/java/com/myappfasttrack/MainActivity.java
@@ -3,12 +3,34 @@ package com.myappfasttrack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtEmail;
+    Button btnLogin;
+    ImageView imgLogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtEmail = findViewById(R.id.edt_email);
+        btnLogin = findViewById(R.id.btn_login);
+        imgLogo = findViewById(R.id.netflix_logo);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgLogo.setImageResource(R.drawable.btn_bg);
+                String strEmail = edtEmail.getText().toString();
+                if (strEmail.equals("")){
+                    Toast.makeText(MainActivity.this, "Please enter Email id", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Email id is "+strEmail, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
