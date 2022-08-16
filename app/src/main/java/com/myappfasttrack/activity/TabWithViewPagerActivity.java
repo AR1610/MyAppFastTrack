69151ea1acec4bf585a4e2490ee448343c6e93ff
Aarti Patel <royal.arati10@gmail.com>
2022-08-16 17:19:22 +0530

    TabLayout with ViewPager final

--- app/src/main/java/com/myappfasttrack/activity/TabWithViewPagerActivity.java
+++ app/src/main/java/com/myappfasttrack/activity/TabWithViewPagerActivity.java
@@ -30,5 +30,21 @@ public class TabWithViewPagerActivity extends AppCompatActivity {
         MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
         viewPager.setAdapter(myPagerAdapter);
 
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 }
